61e8e2457360ec5e61dc2b55598a4bfdc2960962
Pongsak Suvanpong <psksvp@gmail.com>
2018-07-29 18:47:13 +1000

    add combo box to select algorithm

--- Signature/AppDelegate.swift
+++ Signature/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: NSObject, NSApplicationDelegate
   @IBOutlet weak var window: NSWindow!
   @IBOutlet weak var outputTextField: NSTextField!
   @IBOutlet weak var fileDropTextField: FileDropTextField!
+  @IBOutlet weak var algorithmsSelector: NSComboBox!
   
   let digests = ["gost-mac","md4",
                   "md5","md_gost94","ripemd160","sha",
@@ -31,6 +32,9 @@ class AppDelegate: NSObject, NSApplicationDelegate
     // Insert code here to initialize your application
     outputTextField.isEditable = false
     fileDropTextField.stringValue = "Drop a file here"
+    algorithmsSelector.removeAllItems()
+    algorithmsSelector.addItems(withObjectValues: digests)
+    algorithmsSelector.selectItem(at: 8)
   }
 
   func applicationWillTerminate(_ aNotification: Notification)
@@ -38,34 +42,39 @@ class AppDelegate: NSObject, NSApplicationDelegate
     // Insert code here to tear down your application
   }
 
+  @IBAction func algorithmSelected(_ sender: Any)
+  {
+    NSLog("algorithm selected :")
+    NSLog(digests[algorithmsSelector.indexOfSelectedItem])
+  }
+  
   @IBAction func generateButtonPushed(_ sender: NSButton)
   {
     if self.fileDropTextField.stringValue == "Drop a file here"
     {
+      self.outputTextField.stringValue = "No file dropped.."
       return
     }
   
     outputTextField.stringValue = ""
-    for d in digests
+    let d = digests[algorithmsSelector.indexOfSelectedItem]
+    do
     {
-      do
+      try Spawn(args: ["/usr/bin/openssl", d, fileDropTextField.stringValue])
       {
-        try Spawn(args: ["/usr/bin/openssl", d, fileDropTextField.stringValue])
+        str in
+        if let index = str.index(of:"=")
         {
-          str in
-          if let index = str.index(of:"=")
-          {
-            let r = d + str[index...]
-            self.outputTextField.stringValue = self.outputTextField.stringValue + r
-          }
+          let r = d + str[index...]
+          self.outputTextField.stringValue = self.outputTextField.stringValue + r
         }
       }
-      catch
-      {
-          self.outputTextField.stringValue = "Error spawning openssl process.."
-      }
-      
     }
+    catch
+    {
+      self.outputTextField.stringValue = "Error spawning openssl process.."
+    }
+    
   }
 }
 
